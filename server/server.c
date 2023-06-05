fc43d5172e9a84ac9254aaf5ea0d5668e4d37a67
Shakime Sparks <jeykimers.johnson@gmail.com>
2023-06-05 16:01:41 -0400

    add: modularized server

--- server/server.c
+++ server/server.c
@@ -10,9 +10,9 @@
 
 #define BUFFER_SIZE 1024
 
-int sockfd; // socket
-char *ip;   // The IP address of the server
-int port;   // The port number used by the server
+int sockfd;                     // Global socket file descriptor
+struct sockaddr_in client_addr; // Global client address
+int len;                        // Global address length
 
 // sudo ufw allow 8080
 // sudo ufw enable
@@ -41,26 +41,12 @@ void handle_shut_down(int sig)
 //    3.  Wait until the datagram packet arrives from the client.
 //    4.  Process the datagram packet and send a reply to the client.
 //    5.  Go back to Step 3.
-
-int main(int argc, char *argv[])
+void createServer(int port)
 {
-
-    // Validate arguments
-    if (argc != 3)
-    {
-        printf("⭐ Usage: %s <port> <ip>\n", argv[0]);
-        return 1;
-    }
-
-    // Get the parameters from the server and store them in global variables
-    ip = argv[1];
-    port = atoi(argv[2]);
-
-    // Set server needed variables
     int n;
     char server_message[BUFFER_SIZE], client_message[BUFFER_SIZE];
-    struct sockaddr_in server_addr, client_addr;
-    int len = sizeof(client_addr);
+    struct sockaddr_in server_addr;
+    len = sizeof(client_addr);
 
     // Clean buffers
     memset(&server_addr, 0, sizeof(server_addr));
@@ -72,38 +58,36 @@ int main(int argc, char *argv[])
     {
         bold_red();
         printf("⛔ Socket creation failed.\n");
-        default_color();
         exit(EXIT_FAILURE);
     }
-
     bold_blue();
     printf("\n🔘 Socket created");
     default_color();
 
     // Configure server address
     server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr(ip); // Use the provided ip
-    server_addr.sin_port = htons(port);          // Use the provided port
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(port); // Use the provided port
 
     // Bind the socket with the server address
     if (bind(sockfd, (const struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
     {
         bold_red();
         printf("\n⛔ Binding failed.\n");
-        default_color();
         exit(EXIT_FAILURE);
     }
-
     bold_white();
     printf("\n💬 Listening... %d\n", port); // Print the port
     default_color();
+}
+
+void handleMessage()
+{
+    int n;
+    char server_message[BUFFER_SIZE], client_message[BUFFER_SIZE];
 
     while (1)
     {
-        // Handle termination
-        signal(SIGINT, handle_shut_down);  // Set up a signal handler for Ctrl+C
-        signal(SIGTSTP, handle_shut_down); // Set up a signal handler for Ctrl+Z
-
         // Receive client's message:
         n = recvfrom(sockfd, client_message, sizeof(client_message), 0, (struct sockaddr *)&client_addr, &len);
         client_message[n] = '\0'; // Null-terminate the received message
@@ -111,14 +95,15 @@ int main(int argc, char *argv[])
         {
             bold_red();
             printf("\n⛔ Couldn't receive.\n");
-            default_color();
             exit(EXIT_FAILURE);
         }
+
         bold_green();
         printf("\n   ● From client: %s\n", client_message);
         default_color();
 
         // Process the message (You can add your own logic here)
+        // For now it's just copying the input to the output
         strcpy(server_message, client_message);
 
         // Send a response to the client
@@ -127,6 +112,7 @@ int main(int argc, char *argv[])
             bold_red();
             printf("\n⛔ Couldn't send.\n");
             default_color();
+
             exit(EXIT_FAILURE);
         }
 
@@ -134,6 +120,29 @@ int main(int argc, char *argv[])
         printf("      ↪ To: %s\n", server_message);
         default_color();
     }
+}
+
+int main(int argc, char *argv[])
+{
+    // Validate arguments
+    if (argc != 2)
+    {
+        bold_yellow();
+        printf("⭐ Usage: %s <port> <ip>\n", argv[0]);
+        default_color();
+        return 1;
+    }
+
+    // Get the parameters from the server and store them in global variables
+    int port = atoi(argv[1]);
+
+    // Handle termination
+    signal(SIGINT, handle_shut_down);  // Set up a signal handler for Ctrl+C
+    signal(SIGTSTP, handle_shut_down); // Set up a signal handler for Ctrl+Z
+
+    // Create server and handle messages
+    createServer(port);
+    handleMessage();
 
     close(sockfd);
 
